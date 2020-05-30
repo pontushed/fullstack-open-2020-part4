f25d1a713e0a746f6524f84fdf726a0edec13777
Pontus <pontus.hedlund@gmail.com>
2020-05-30 16:23:57 +0300

    Changes for Part 5 compatibility

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,11 +2,10 @@ const testingRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-testingRouter.post('/reset', async (response) => {
+testingRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
-  console.log('Database reset for testing')
-  response.status(200).end()
+  response.status(204).send('Database is now reset for testing').end()
 })
 
 module.exports = testingRouter
